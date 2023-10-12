770673ef154873ea7927ae2862ffb9de10fb2cab
Deep-khokhar <deepkhokhar1262004@gmail.com>
2023-10-12 21:11:43 +0530

    HOME PAGE UPDATE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,145 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import "../assets/style/home.css";
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <>
+        <div className="Home-main">
+          {/* home-first-div */}
+          <div className="row featurette">
+            <div className="col-md-7 m-auto home-first-div">
+              <h2 className="featurette-heading home-all-line-height fw-normal ">
+                First featurette heading.
+                <span className="text-body-secondary">
+                  It’ll blow your mind.
+                </span>
+              </h2>
+              <p className="lead">
+                Some great placeholder content for the first featurette here.
+                Imagine some exciting prose here.
+              </p>
+            <button>sdv</button>
+            </div>
+            <div className="col-md-5">
+              <svg
+                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                width={500}
+                height={500}
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 500x500"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-bg)"
+                />
+                <text
+                  x="50%"
+                  y="50%"
+                  fill="var(--bs-secondary-color)"
+                  dy=".3em"
+                >
+                  500x500
+                </text>
+              </svg>
+            
+            </div>
+          </div>
+          <hr className="" />
+
+          {/* home-second-div */}
+          <div className="row featurette m-auto">
+            <div className="col-md-7 order-md-2 m-auto home-second-div">
+              <h2 className="featurette-heading fw-normal home-all-line-height">
+                Oh yeah, it’s that good.{" "}
+                <span className="text-body-secondary">See for yourself.</span>
+              </h2>
+              <p className="lead">
+                Another featurette? Of course. More placeholder content here to
+                give you an idea of how this layout would work with some actual
+                real-world content in place.
+              </p>
+            </div>
+            <div className="col-md-5 order-md-1 p-5">
+              <svg
+                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                width={500}
+                height={500}
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 500x500"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-bg)"
+                />
+                <text
+                  x="50%"
+                  y="50%"
+                  fill="var(--bs-secondary-color)"
+                  dy=".3em"
+                >
+                  500x500
+                </text>
+              </svg>
+            </div>
+          </div>
+          <hr className="" />
+
+          {/* home-third-div */}
+          <div className="row featurette">
+            <div className="col-md-7 m-auto home-third-div">
+              <h2 className="featurette-heading fw-normal home-all-line-height">
+                And lastly, this one.{" "}
+                <span className="text-body-secondary">Checkmate.</span>
+              </h2>
+              <p className="lead">
+                And yes, this is the last block of representative placeholder
+                content. Again, not really intended to be actually read, simply
+                here to give you a better view of what this would look like with
+                some actual content. Your content.
+              </p>
+            </div>
+            <div className="col-md-5">
+              <svg
+                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                width={500}
+                height={500}
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 500x500"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect
+                  width="100%"
+                  height="100%"
+                  fill="var(--bs-secondary-bg)"
+                />
+                <text
+                  x="50%"
+                  y="50%"
+                  fill="var(--bs-secondary-color)"
+                  dy=".3em"
+                >
+                  500x500
+                </text>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </>
+    </Layout>
+  );
 };
 
 export default Home;
